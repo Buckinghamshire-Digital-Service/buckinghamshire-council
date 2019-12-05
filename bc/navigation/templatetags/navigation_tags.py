f5ef1b7413ed7fa0df24e45e24e3865b6ea19067
Victoria Chan <victoria.chan@torchbox.com>
2019-12-05 10:10:02 +0000

    Remove primary, secondary, and footer navigation code

--- bc/navigation/templatetags/navigation_tags.py
+++ bc/navigation/templatetags/navigation_tags.py
@@ -5,43 +5,7 @@ from bc.navigation.models import NavigationSettings
 register = template.Library()
 
 
-# Primary nav snippets
-@register.inclusion_tag(
-    "patterns/molecules/navigation/primarynav.html", takes_context=True
-)
-def primarynav(context):
-    request = context["request"]
-    return {
-        "primarynav": NavigationSettings.for_site(request.site).primary_navigation,
-        "request": request,
-    }
-
-
-# Secondary nav snippets
-@register.inclusion_tag(
-    "patterns/molecules/navigation/secondarynav.html", takes_context=True
-)
-def secondarynav(context):
-    request = context["request"]
-    return {
-        "secondarynav": NavigationSettings.for_site(request.site).secondary_navigation,
-        "request": request,
-    }
-
-
-# Footer nav snippets
-@register.inclusion_tag(
-    "patterns/molecules/navigation/footernav.html", takes_context=True
-)
-def footernav(context):
-    request = context["request"]
-    return {
-        "footernav": NavigationSettings.for_site(request.site).footer_navigation,
-        "request": request,
-    }
-
-
-# Footer nav snippets
+# Sidebar snippets
 @register.inclusion_tag(
     "patterns/molecules/navigation/sidebar.html", takes_context=True
 )
