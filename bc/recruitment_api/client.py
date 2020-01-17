d45adaa478628132f90da997315580054d757e0c
Nick Smith <nick.smith@torchbox.com>
2020-01-17 13:39:12 +0000

    Do not run Zeep with custom settings

--- bc/recruitment_api/client.py
+++ bc/recruitment_api/client.py
@@ -1,19 +1,16 @@
 from django.conf import settings
 
-from zeep import Client, Settings
+from zeep import Client
 from zeep.wsse import UsernameToken
 
 from bc.recruitment_api.cache import ZeepDjangoBackendCache
 from bc.recruitment_api.constants import TALENTLINK_API_WSDL
 from bc.recruitment_api.transports import ZeepAPIKeyTransport
 
-zeep_settings = Settings(strict=False, xml_huge_tree=True)
-
 
 def get_client(wsdl=TALENTLINK_API_WSDL):
     return Client(
         wsdl,
-        settings=zeep_settings,
         transport=ZeepAPIKeyTransport(
             cache=ZeepDjangoBackendCache(), api_key=settings.TALENTLINK_API_KEY
         ),
