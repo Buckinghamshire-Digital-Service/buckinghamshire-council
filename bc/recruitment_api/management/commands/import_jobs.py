0e60eb97fcb1b841812be0e43b5572e088fb146f
Nick Smith <nick.smith@torchbox.com>
2020-01-30 09:37:58 +0000

    Add basic job import error handling
    - Import errors are now caught, not imported, and reported to stdout.
    - Also refactor import fixture to a `get_advertisment()`
    - Also add tests of error handling

--- bc/recruitment_api/management/commands/import_jobs.py
+++ bc/recruitment_api/management/commands/import_jobs.py
@@ -13,8 +13,9 @@ class Command(BaseCommand):
         client = get_client()
         page = 1
         results = True
-        updated = 0
-        created = 0
+        num_updated = 0
+        num_created = 0
+        errors = []
         while results:
             self.stdout.write(f"Fetching page {page}")
             response = client.service.getAdvertisementsByPage(page)
@@ -26,12 +27,30 @@ class Command(BaseCommand):
 
                     try:
                         job = TalentLinkJob.objects.get(talentlink_id=ad["id"])
-                        updated += 1
+                        created = False
                     except TalentLinkJob.DoesNotExist:
                         job = TalentLinkJob(talentlink_id=ad["id"])
-                        created += 1
+                        created = True
 
-                    job = update_job_from_ad(job, ad, defaults={"last_imported": now()})
+                    try:
+                        job = update_job_from_ad(
+                            job, ad, defaults={"last_imported": now()}
+                        )
+                    except Exception as e:
+                        msg = (
+                            f"Error occurred while processing job {ad['id']}:\n"
+                            + str(e)
+                        )
+                        errors.append(msg)
+                    else:
+                        if created:
+                            num_created += 1
+                        else:
+                            num_updated += 1
+
         self.stdout.write("No more results")
-        self.stdout.write(f"{updated} existing jobs updated")
-        self.stdout.write(f"{created} new jobs created")
+        self.stdout.write(f"{num_updated} existing jobs updated")
+        self.stdout.write(f"{num_created} new jobs created")
+        self.stdout.write(self.style.ERROR(f"{len(errors)} errors"))
+        for error in errors:
+            self.stdout.write(self.style.ERROR(msg))
--- bc/recruitment_api/tests/test_imports.py
+++ bc/recruitment_api/tests/test_imports.py
@@ -9,16 +9,18 @@ from freezegun import freeze_time
 
 from bc.recruitment.models import TalentLinkJob
 from bc.recruitment.tests.fixtures import TalentLinkJobFactory
+from bc.recruitment_api.utils import update_job_from_ad
 
-from .fixtures import advertisement, no_further_pages_response
+from .fixtures import get_advertisement, no_further_pages_response
 
 
 @mock.patch("bc.recruitment_api.management.commands.import_jobs.get_client")
 class ImportTest(TestCase):
-    def get_mocked_client(self):
+    def get_mocked_client(self, advertisements=None):
+        advertisements = advertisements or [get_advertisement()]
         client = mock.Mock()
         client.service.getAdvertisementsByPage.side_effect = [
-            {"advertisements": {"advertisement": [advertisement]}, "totalResults": 143},
+            {"advertisements": {"advertisement": advertisements}, "totalResults": 143},
             no_further_pages_response,
         ]
         return client
@@ -96,3 +98,60 @@ class ImportTest(TestCase):
 
         job.refresh_from_db()
         self.assertEqual(job.last_imported, later)
+
+    @mock.patch("bc.recruitment_api.management.commands.import_jobs.update_job_from_ad")
+    def test_errors_are_reported(self, mock_update_fn, mock_get_client):
+        error_message = "This is a test error message"
+        mock_update_fn.side_effect = KeyError(error_message)
+
+        mock_get_client.return_value = self.get_mocked_client()
+
+        out = StringIO()
+        call_command("import_jobs", stdout=out)
+        out.seek(0)
+        output = out.read()
+        self.assertIn(error_message, output)
+
+    @mock.patch("bc.recruitment_api.management.commands.import_jobs.update_job_from_ad")
+    def test_error_cases_are_not_imported(self, mock_update_fn, mock_get_client):
+        instant = datetime.datetime(2020, 1, 29, 12, 0, 0, tzinfo=datetime.timezone.utc)
+        job_1 = TalentLinkJobFactory(
+            talentlink_id=1, title="Original title 1", last_imported=instant
+        )
+        job_2 = TalentLinkJobFactory(
+            talentlink_id=2, title="Original title 2", last_imported=instant
+        )
+
+        error_message = "This is a test error message"
+
+        def error_or_original(job, ad, defaults):
+            """ Raise an error for id 1 only"""
+            if job.talentlink_id == 1:
+                raise KeyError(error_message)
+            else:
+                return update_job_from_ad(job, ad, defaults)
+
+        mock_update_fn.side_effect = error_or_original
+
+        advertisements = [
+            get_advertisement(talentlink_id=1, title="New title 1"),
+            get_advertisement(talentlink_id=2, title="New title 2"),
+        ]
+        mock_get_client.return_value = self.get_mocked_client(advertisements)
+
+        out = StringIO()
+        later = instant + datetime.timedelta(days=1)
+        with freeze_time(later):
+            call_command("import_jobs", stdout=out)
+        out.seek(0)
+        output = out.read()
+        self.assertIn(error_message, output)
+        self.assertIn("1 existing jobs updated", output)
+        self.assertIn("1 errors", output)
+
+        job_1.refresh_from_db()
+        self.assertEqual(job_1.title, "Original title 1")  # not updated
+        self.assertEqual(job_1.last_imported, instant)  # not updated
+        job_2.refresh_from_db()
+        self.assertEqual(job_2.title, "New title 2")  # job 2 has been updated
+        self.assertEqual(job_2.last_imported, later)  # job 2 has been updated
