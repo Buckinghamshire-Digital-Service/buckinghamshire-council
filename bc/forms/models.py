ccf80958df3befecce2d090339bddd0203bcb22c
Nick Smith <nick.smith@torchbox.com>
2020-03-20 16:30:28 +0000

    Convert form heading fields to field headings

--- bc/forms/models.py
+++ bc/forms/models.py
@@ -11,7 +11,7 @@ from wagtail.admin.edit_handlers import (
     MultiFieldPanel,
 )
 from wagtail.contrib.forms.forms import FormBuilder
-from wagtail.contrib.forms.models import FORM_FIELD_CHOICES, AbstractFormField
+from wagtail.contrib.forms.models import AbstractFormField
 from wagtail.core.fields import RichTextField
 from wagtail.search import index
 
@@ -19,41 +19,22 @@ from wagtailcaptcha.models import WagtailCaptchaEmailForm
 
 from bc.utils.constants import RICH_TEXT_FEATURES
 from bc.utils.models import BasePage
-from bc.utils.widgets import (
-    CustomCheckboxSelectMultiple,
-    CustomHeadingField,
-    CustomSubheadingField,
-)
+from bc.utils.widgets import CustomCheckboxSelectMultiple
 
 
 class FormField(AbstractFormField):
     page = ParentalKey("FormPage", related_name="form_fields")
-    CHOICES = FORM_FIELD_CHOICES + (
-        ("heading", "Heading"),
-        ("subheading", "Subheading"),
+
+    additional_text = RichTextField(
+        blank=True, help_text="Rich text to display before the form field"
     )
 
-    # Override the field_type field with extended choices
-    field_type = models.CharField(
-        verbose_name="field type", max_length=16, choices=CHOICES
-    )
+    panels = AbstractFormField.panels + [FieldPanel("additional_text")]
 
 
 class CustomFormBuilder(FormBuilder):
     # create a function that returns an instanced Django form field
     # function name must match create_<field_type_key>_field
-    def create_heading_field(self, field, options):
-        options["initial"] = options["label"]
-        options["required"] = False
-
-        return forms.Field(widget=CustomHeadingField, **options)
-
-    def create_subheading_field(self, field, options):
-        options["initial"] = options["label"]
-        options["required"] = False
-
-        return forms.Field(widget=CustomSubheadingField, **options)
-
     def create_checkboxes_field(self, field, options):
         # Based on code in wagtail.contrib.forms, but changing widget
         options["choices"] = [(x.strip(), x.strip()) for x in field.choices.split(",")]
--- bc/utils/widgets.py
+++ bc/utils/widgets.py
@@ -1,16 +1,7 @@
 from django import forms
-from django.forms.widgets import Widget
 
 
 class CustomCheckboxSelectMultiple(forms.widgets.CheckboxSelectMultiple):
     template_name = (
         "patterns/molecules/form-widgets/custom_checkbox_select_multiple.html"
     )
-
-
-class CustomHeadingField(Widget):
-    template_name = "patterns/molecules/form-widgets/heading.html"
-
-
-class CustomSubheadingField(Widget):
-    template_name = "patterns/molecules/form-widgets/subheading.html"
