9995c0543d66b0907839e3773d9b40afd1af247c
Victoria Chan <victoria.chan@torchbox.com>
2020-03-24 14:12:02 +0000

    Import location data

--- bc/recruitment_api/utils.py
+++ bc/recruitment_api/utils.py
@@ -129,9 +129,10 @@ def update_job_from_ad(job, ad, defaults=None, import_categories=False):
     # Get location data
     if ad["jobLocations"]:
         location = ad["jobLocations"]["jobLocation"][0]
-        job.location_postcode = location["zipCode"]
-        job.location_lat = location["latitude"]
-        job.location_lon = location["longitude"]
+        if location["zipCode"]:
+            job.location_postcode = location["zipCode"]
+            job.location_lat = location["latitude"]
+            job.location_lon = location["longitude"]
 
     job.save()
     return job
