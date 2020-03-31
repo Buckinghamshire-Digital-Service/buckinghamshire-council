1ea7e5922daff0b760702711f53128fd460f1d31
Victoria Chan <victoria.chan@torchbox.com>
2020-03-31 11:49:13 +0100

    linting

--- bc/recruitment/migrations/0020_job_postcode.py
+++ bc/recruitment/migrations/0020_job_postcode.py
@@ -6,23 +6,27 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('recruitment', '0019_merge_20200325_1600'),
+        ("recruitment", "0019_merge_20200325_1600"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='talentlinkjob',
-            name='location_lat',
-            field=models.DecimalField(blank=True, decimal_places=6, max_digits=9, null=True),
+            model_name="talentlinkjob",
+            name="location_lat",
+            field=models.DecimalField(
+                blank=True, decimal_places=6, max_digits=9, null=True
+            ),
         ),
         migrations.AddField(
-            model_name='talentlinkjob',
-            name='location_lon',
-            field=models.DecimalField(blank=True, decimal_places=6, max_digits=9, null=True),
+            model_name="talentlinkjob",
+            name="location_lon",
+            field=models.DecimalField(
+                blank=True, decimal_places=6, max_digits=9, null=True
+            ),
         ),
         migrations.AddField(
-            model_name='talentlinkjob',
-            name='location_postcode',
+            model_name="talentlinkjob",
+            name="location_postcode",
             field=models.CharField(blank=True, max_length=8),
         ),
     ]
