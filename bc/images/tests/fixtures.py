27d9a3523afaa414594bc7cfa0ba2d5e9d00ad56
Victoria Chan <victoria.chan@torchbox.com>
2020-04-09 14:51:34 +0100

    Remove unused mock

--- bc/images/tests/fixtures.py
+++ bc/images/tests/fixtures.py
@@ -12,29 +12,6 @@ class ImageFactory(factory.django.DjangoModelFactory):
         model = "images.CustomImage"
 
 
-# VC TODO: test import_image_from_url
-# For CustomImage import_image_from_url()
-def mock_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-
-    if args[0] == "https://test.url/test.png":
-        return MockResponse(
-            {
-                "headers": {"Content-Type": "image/png"},
-                "content": "incomprehensiblefilecontent",
-            },
-            200,
-        )
-
-    return MockResponse(None, 404)
-
-
 def mock_import_image_from_url(
     title, url, filename, talentlink_image_id=None, collection_name="imported"
 ):
