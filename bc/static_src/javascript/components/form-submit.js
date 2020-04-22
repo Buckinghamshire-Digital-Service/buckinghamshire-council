9d1f4eed1fe2813c90bc0efa4af799c0795e1f3b
jane.hughes <jane.hughes@torchbox.com>
2020-04-22 12:07:06 +0100

    add explanatory comments & styling for disabled button

--- bc/static_src/javascript/components/form-submit.js
+++ bc/static_src/javascript/components/form-submit.js
@@ -10,9 +10,13 @@ class FormSubmit {
 
     bindEvents() {
         this.form.addEventListener('submit', (e) => {
+            const submitButton = this.querySelector('[type="submit"]');
             const submittedClass = 'js-submitted';
+            // if form contains submitted class, disallow further submissions & add disabled attribute to submit button
             if (this.classList.contains(submittedClass)) {
                 e.preventDefault();
+                submitButton.disabled = true;
+            // if form does not contain submitted class (first submission), add it
             } else {
                 this.classList.add(submittedClass);
             }
