007882c2b8024d2a0551cf1d7cfdc3fa7038b538
Victoria Chan <victoria.chan@torchbox.com>
2020-04-24 13:32:41 +0100

    Fixed tests

--- bc/recruitment/migrations/0032_set_default_job_homepage.py
+++ bc/recruitment/migrations/0032_set_default_job_homepage.py
@@ -4,14 +4,16 @@ from django.db import migrations
 def set_default_job_homepage(apps, schema_editor):
     RecruitmentHomePage = apps.get_model("recruitment", "RecruitmentHomePage")
     home = RecruitmentHomePage.objects.first()
-    home.job_board = "external"
-    home.save()
 
-    TalentLinkJob = apps.get_model("recruitment", "TalentLinkJob")
-    TalentLinkJob.objects.filter(homepage=None).update(homepage=home)
+    if home:
+        home.job_board = "external"
+        home.save()
 
-    JobAlertSubscription = apps.get_model("recruitment", "JobAlertSubscription")
-    JobAlertSubscription.objects.filter(homepage=None).update(homepage=home)
+        TalentLinkJob = apps.get_model("recruitment", "TalentLinkJob")
+        TalentLinkJob.objects.filter(homepage=None).update(homepage=home)
+
+        JobAlertSubscription = apps.get_model("recruitment", "JobAlertSubscription")
+        JobAlertSubscription.objects.filter(homepage=None).update(homepage=home)
 
 
 class Migration(migrations.Migration):
