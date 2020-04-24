ae769861ecbe880bda39f97b178a20dba7639ed0
Victoria Chan <victoria.chan@torchbox.com>
2020-04-24 12:36:20 +0100

    Add migration to set default job board for existing homepage

--- bc/recruitment/migrations/0032_set_default_job_homepage.py
+++ bc/recruitment/migrations/0032_set_default_job_homepage.py
@@ -4,6 +4,8 @@ from django.db import migrations
 def set_default_job_homepage(apps, schema_editor):
     RecruitmentHomePage = apps.get_model("recruitment", "RecruitmentHomePage")
     home = RecruitmentHomePage.objects.first()
+    home.job_board = "external"
+    home.save()
 
     TalentLinkJob = apps.get_model("recruitment", "TalentLinkJob")
     TalentLinkJob.objects.filter(homepage=None).update(homepage=home)
