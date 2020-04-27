370bdf38bcf04b421cd80b4aaefb45992d0d2727
jane.hughes <jane.hughes@torchbox.com>
2020-04-27 11:59:29 +0100

    update double submissions fix

--- bc/static_src/javascript/components/form-submit.js
+++ bc/static_src/javascript/components/form-submit.js
@@ -10,15 +10,15 @@ class FormSubmit {
 
     bindEvents() {
         this.form.addEventListener('submit', (e) => {
-            const submitButton = this.querySelector('[type="submit"]');
             const submittedClass = 'js-submitted';
-            // if form contains submitted class, disallow further submissions & add disabled attribute to submit button
-            if (this.classList.contains(submittedClass)) {
+
+            // if form contains submitted class, don't allow further submissions
+            if (this.form.classList.contains(submittedClass)) {
                 e.preventDefault();
-                submitButton.disabled = true;
-                // if form does not contain submitted class (first submission), add it
+                // if form does not contain submitted class (first submission), add it and disable submit button
             } else {
-                this.classList.add(submittedClass);
+                submitButton.disabled = true;
+                this.form.classList.add(submittedClass);
             }
         });
     }
