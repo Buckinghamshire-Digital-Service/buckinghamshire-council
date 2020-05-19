a0655d92e1cb849269bd6ed9bda29ddc6fd4c886
jane.hughes <jane.hughes@torchbox.com>
2020-05-19 12:45:21 +0100

    update GTM code

--- bc/utils/context_processors.py
+++ bc/utils/context_processors.py
@@ -4,6 +4,10 @@ from django.conf import settings
 def global_vars(request):
     return {
         "GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None),
+        "GOOGLE_TAG_MANAGER_AUTH": getattr(settings, "GOOGLE_TAG_MANAGER_AUTH", None),
+        "GOOGLE_TAG_MANAGER_ENV_ID": getattr(
+            settings, "GOOGLE_TAG_MANAGER_ENV_ID", None
+        ),
         "YANDEX_VERIFICATION_STRING": getattr(
             settings, "YANDEX_VERIFICATION_STRING", None
         ),
