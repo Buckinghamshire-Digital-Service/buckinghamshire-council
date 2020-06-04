7a351491d4dd0a9d7cf7a13d7bdc93fc48c9cfa8
Nick Smith <nick.smith@torchbox.com>
2020-06-04 11:12:41 +0100

    Remove GTM _AUTH and _ENV_ID settings

--- bc/utils/context_processors.py
+++ bc/utils/context_processors.py
@@ -4,10 +4,6 @@ from django.conf import settings
 def global_vars(request):
     return {
         "GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None),
-        "GOOGLE_TAG_MANAGER_AUTH": getattr(settings, "GOOGLE_TAG_MANAGER_AUTH", None),
-        "GOOGLE_TAG_MANAGER_ENV_ID": getattr(
-            settings, "GOOGLE_TAG_MANAGER_ENV_ID", None
-        ),
         "YANDEX_VERIFICATION_STRING": getattr(
             settings, "YANDEX_VERIFICATION_STRING", None
         ),
