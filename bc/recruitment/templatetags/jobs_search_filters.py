793ff973569cfb561e96fbaf03dce5982ac229eb
Nick Smith <nick.smith@torchbox.com>
2020-06-05 17:18:46 +0100

    Remove Wagtail SiteMiddleware
    SiteMiddleware, and therefore request.site, is [deprecated in Wagtail 2.9][1]
    
    [1]: https://docs.wagtail.io/en/v2.9/releases/2.9.html#sitemiddleware-and-request-site-deprecated

--- bc/recruitment/templatetags/jobs_search_filters.py
+++ bc/recruitment/templatetags/jobs_search_filters.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db.models import F
 
+from wagtail.core.models import Site
+
 from bc.recruitment.models import JobCategory, TalentLinkJob
 from bc.recruitment.utils import get_school_and_early_years_count
 
@@ -10,7 +12,7 @@ register = template.Library()
 @register.inclusion_tag("patterns/molecules/search-filters/search-filters.html")
 def jobs_search_filters(request, unfiltered_results=None):
     search_postcode = request.GET.get("postcode", None)
-    homepage = request.site.root_page.specific
+    homepage = Site.find_for_request(request).root_page.specific
 
     if not unfiltered_results:
         # Provide a default queryset for Pattern Library
