847e60760a048cf5d10634fc8510f3369de8c82d
Nick Smith <nick.smith@torchbox.com>
2020-07-20 14:30:55 +0100

    Add LinkedIn and Instagram social media settings and icons

--- bc/utils/tests/test_validators.py
+++ bc/utils/tests/test_validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from ..validators import validate_youtube_domain
+from ..validators import validate_linkedin_domain, validate_youtube_domain
 
 
 class YouTubeURLValidatorTest(TestCase):
@@ -28,3 +28,27 @@ class YouTubeURLValidatorTest(TestCase):
             with self.subTest(url=value):
                 with self.assertRaises(ValidationError):
                     validate_youtube_domain(value)
+
+
+class LinkedInURLValidatorTest(TestCase):
+    def test_valid_values_are_validated(self):
+        values = [
+            "http://www.linkedin.com/",
+            "https://www.linkedin.com/",
+            "http://www.linkedin.com/bc",
+            "http://www.linkedin.com/bc/",
+            "https://www.linkedin.com/bc",
+            "https://www.linkedin.com/bc/",
+            "https://www.linkedin.com/c/bc",
+            "https://www.linkedin.com/c/bc/",
+        ]
+        for value in values:
+            with self.subTest(url=value):
+                validate_linkedin_domain(value)
+
+    def test_invalid_values_are_not_validated(self):
+        values = ["https://linked.in/", "https://www.linkedin.org/"]
+        for value in values:
+            with self.subTest(url=value):
+                with self.assertRaises(ValidationError):
+                    validate_linkedin_domain(value)
