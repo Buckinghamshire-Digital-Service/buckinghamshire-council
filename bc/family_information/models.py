8fbef7ff597997c45882cb76a95ad6c834097bae
Nick Smith <nick.smith@torchbox.com>
2020-10-27 17:34:47 +0000

    Rewrite FIS models
    This includes models but not templates for CategoryTypeOnePage and
    CategoryTypeTwoPage.

--- bc/family_information/models.py
+++ bc/family_information/models.py
@@ -1,26 +1,60 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils.functional import cached_property
 
-from modelcluster.fields import ParentalKey
-from wagtail.admin.edit_handlers import (
-    FieldPanel,
-    InlinePanel,
-    MultiFieldPanel,
-    PageChooserPanel,
-)
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
-from ..utils.models import BasePage, Orderable
+from ..utils.models import BasePage
 
 
-class FamilyInformationHomePage(BasePage):
+class FISBannerFields(models.Model):
+    banner_image = models.ForeignKey(
+        "images.CustomImage", null=True, related_name="+", on_delete=models.SET_NULL,
+    )
+    banner_title = models.TextField()
+    banner_description = models.TextField()
+    banner_link = models.URLField()
+    banner_link_text = models.CharField(max_length=100, blank=True)
+
+    search_fields = [
+        index.SearchField("banner_title"),
+        index.SearchField("banner_description"),
+    ]
+
+    content_panels = [
+        MultiFieldPanel(
+            [
+                ImageChooserPanel("banner_image"),
+                FieldPanel("banner_title"),
+                FieldPanel("banner_description"),
+                FieldPanel("banner_link"),
+                FieldPanel("banner_link_text"),
+            ],
+            heading="Banner",
+        ),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class FamilyInformationHomePage(BasePage, FISBannerFields):
     template = "patterns/pages/family_information/family_information_home_page.html"
+
+    subpage_types = ["CategoryTypeOnePage", "CategoryTypeTwoPage"]
 
     max_count = 1
 
     hero_image = models.ForeignKey(
         "images.CustomImage", null=True, related_name="+", on_delete=models.SET_NULL,
     )
+    description = models.TextField(blank=True)
+    search_placeholder = models.CharField(max_length=100, blank=True)
+
     call_to_action = models.ForeignKey(
         "utils.CallToActionSnippet",
         blank=True,
@@ -28,63 +62,68 @@ class FamilyInformationHomePage(BasePage):
         on_delete=models.SET_NULL,
         related_name="+",
     )
-    links_image = models.ForeignKey(
-        "images.CustomImage", null=True, related_name="+", on_delete=models.SET_NULL,
-    )
-    left_title = models.CharField(max_length=255, null=True)
-    left_lede = models.TextField(null=True)
-    left_label = models.CharField(max_length=255, null=True, blank=True)
-    left_action = models.ForeignKey(
-        "wagtailcore.Page", on_delete=models.SET_NULL, related_name="+", null=True
-    )
-    right_title = models.CharField(max_length=255, null=True)
-    right_lede = models.TextField(null=True)
-    right_label = models.CharField(max_length=255, null=True, blank=True)
-    right_action = models.ForeignKey(
-        "wagtailcore.Page", on_delete=models.SET_NULL, related_name="+", null=True
+
+    search_fields = (
+        BasePage.search_fields
+        + FISBannerFields.search_fields
+        + [index.SearchField("description")]
     )
 
-    content_panels = BasePage.content_panels + [
-        ImageChooserPanel("hero_image"),
-        MultiFieldPanel(
-            [
-                InlinePanel("useful_links", max_num=2, min_num=0, label="Link"),
-                ImageChooserPanel("links_image"),
-            ],
-            heading="Useful links",
-        ),
-        SnippetChooserPanel("call_to_action"),
-        MultiFieldPanel(
-            [
-                FieldPanel("left_title"),
-                FieldPanel("left_lede"),
-                FieldPanel("left_label"),
-                PageChooserPanel("left_action"),
-            ],
-            heading="Left action",
-        ),
-        MultiFieldPanel(
-            [
-                FieldPanel("right_title"),
-                FieldPanel("right_lede"),
-                FieldPanel("right_label"),
-                PageChooserPanel("right_action"),
-            ],
-            heading="Right action",
-        ),
-    ]
-
-
-class UsefulLinks(Orderable):
-    page = ParentalKey(
-        "family_information.FamilyInformationHomePage", related_name="useful_links"
+    content_panels = (
+        BasePage.content_panels
+        + [
+            MultiFieldPanel(
+                [
+                    ImageChooserPanel("hero_image"),
+                    FieldPanel("description"),
+                    FieldPanel("search_placeholder"),
+                ],
+                heading="Hero",
+            ),
+        ]
+        + FISBannerFields.content_panels
+        + [SnippetChooserPanel("call_to_action")]
     )
-    title = models.CharField(max_length=255)
-    lede = models.TextField()
-    url = models.URLField()
 
-    content_panels = [
-        FieldPanel("title"),
-        FieldPanel("lede"),
-        FieldPanel("url"),
-    ]
+    @cached_property
+    def category_pages(self):
+        """Get category pages for the homepage listing.
+
+        Returns a queryset of this page's live, public children of either of the two
+        CategoryTypeX classes, ordered by Wagtail explorer custom sort (ie. path).
+        """
+        return (
+            Page.objects.child_of(self)
+            .filter(
+                content_type__in=ContentType.objects.get_for_models(
+                    CategoryTypeOnePage, CategoryTypeTwoPage
+                ).values()
+            )
+            .filter(show_in_menus=True)
+            .live()
+            .public()
+            .order_by("path")
+        )
+
+
+class BaseCategoryPage(Page, FISBannerFields):
+    parent_page_types = ["FamilyInformationHomePage"]
+    subpage_types = ["inlineindex.InlineIndex"]
+
+    content_panels = BasePage.content_panels + FISBannerFields.content_panels
+    search_fields = BasePage.search_fields + FISBannerFields.search_fields
+
+    class Meta:
+        abstract = True
+
+    @cached_property
+    def child_pages(self):
+        return self.get_children().live().public().specific().order_by("path")
+
+
+class CategoryTypeOnePage(BaseCategoryPage):
+    template = "patterns/pages/family_information/category_type_1.html"
+
+
+class CategoryTypeTwoPage(BaseCategoryPage):
+    template = "patterns/pages/family_information/category_type_2.html"
