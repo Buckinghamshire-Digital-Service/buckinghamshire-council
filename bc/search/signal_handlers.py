2c6ea114767d5663a1fc955d3dd277e890675613
Nick Smith <nick.smith@torchbox.com>
2020-11-17 11:38:49 +0000

    Fix bad import in signal_handlers

--- bc/search/signal_handlers.py
+++ bc/search/signal_handlers.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from django.search.models import Term
 
+from bc.search.models import Term
 from bc.search.utils import get_synonyms
 
 
