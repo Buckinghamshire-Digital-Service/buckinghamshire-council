b27059ff168d85c76be2b6ce063f03acab5d8f40
Sharmaine Lim <sharmaine@kirigami.co>
2020-11-19 16:35:43 +0800

    FIS Inline Index / Subcategory Page: Remove unused sidebar

--- bc/navigation/templatetags/navigation_tags.py
+++ bc/navigation/templatetags/navigation_tags.py
@@ -5,17 +5,6 @@ from bc.navigation.models import NavigationSettings
 register = template.Library()
 
 
-# Sidebar snippets
-@register.inclusion_tag(
-    "patterns/molecules/navigation/sidebar.html", takes_context=True
-)
-def sidebar(context):
-    return {
-        "children": context["page"].get_children().live().public().in_menu(),
-        "request": context["request"],
-    }
-
-
 # Footer nav snippets
 @register.inclusion_tag(
     "patterns/molecules/navigation/footernav.html", takes_context=True
