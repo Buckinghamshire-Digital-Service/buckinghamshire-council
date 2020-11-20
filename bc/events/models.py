2ce40f2b70dc8cea48dd253c05b57c12750cc8c4
Sharmaine Lim <sharmaine@kirigami.co>
2020-11-20 22:11:28 +0800

    Event Page: Use live_related_pages

--- bc/events/models.py
+++ bc/events/models.py
@@ -110,6 +110,13 @@ class EventPage(BasePage):
         if self.start_date:
             return self.start_date
 
+    @cached_property
+    def live_related_pages(self):
+        return self.related_pages.annotate(
+            # Presence of a page restriction means it's private
+            restriction_count=models.Count("page__view_restrictions")
+        ).filter(page__live=True, restriction_count=0)
+
     @property
     def location(self):
         if self.location_name:
