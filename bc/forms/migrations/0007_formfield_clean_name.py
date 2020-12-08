27d8b562b5c8268bed6d7453b5d8fd7fc835743a
Tibor Leupold <tibor.leupold@torchbox.com>
2020-12-08 10:46:22 -0800

    Fit formatting (black)

--- bc/forms/migrations/0007_formfield_clean_name.py
+++ bc/forms/migrations/0007_formfield_clean_name.py
@@ -6,13 +6,19 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('forms', '0006_add_lookup_page'),
+        ("forms", "0006_add_lookup_page"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='formfield',
-            name='clean_name',
-            field=models.CharField(blank=True, default='', help_text='Safe name of the form field, the label converted to ascii_snake_case', max_length=255, verbose_name='name'),
+            model_name="formfield",
+            name="clean_name",
+            field=models.CharField(
+                blank=True,
+                default="",
+                help_text="Safe name of the form field, the label converted to ascii_snake_case",
+                max_length=255,
+                verbose_name="name",
+            ),
         ),
     ]
