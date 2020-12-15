5153f4c8ae6594d423ad55120f1afaae3ab80ce6
Tibor Leupold <tibor.leupold@torchbox.com>
2020-12-15 14:10:25 -0800

    Add sanity checks to tests
    This is only to make sure that the test setup works and that I am
    creating a draft page properly.

--- bc/inlineindex/tests/tests.py
+++ bc/inlineindex/tests/tests.py
@@ -6,15 +6,36 @@ from bc.inlineindex.tests.fixtures import InlineIndexFactory
 
 
 class TestDisplayOfChildPages(TestCase):
-    def test_previewing_draft_index_shows_draft_children(self):
-        homepage = HomePage.objects.first()
+    def setUp(self):
+        self.homepage = HomePage.objects.first()
 
-        response = self.client.get(homepage.url)
+        response = self.client.get(self.homepage.url)
         self.assertEqual(response.status_code, 200)
 
-        inline_index = InlineIndexFactory(parent=homepage, title="The Example Index")
+    def test_live_request_to_live_index_success(self):
+        """
+        Just a sanity check.
+        """
+        inline_index = InlineIndexFactory(
+            parent=self.homepage, title="The Example Index"
+        )
 
         response = self.client.get(inline_index.url)
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "The Example Index")
+
+    def test_live_request_to_draft_index_fails(self):
+        """
+        Just a sanity check.
+        """
+        inline_index = InlineIndexFactory(
+            parent=self.homepage, title="The Example Index", live=False
+        )
+
+        response = self.client.get(inline_index.url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_previewing_draft_index_shows_draft_children(self):
+        pass
