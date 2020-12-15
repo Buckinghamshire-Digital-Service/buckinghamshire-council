1b976e29133641e09c2ddba5249889f3dd786f5e
Tibor Leupold <tibor.leupold@torchbox.com>
2020-12-15 14:57:43 -0800

    Add tests for desired display functionality
    This adds the proper test to check if the the desired functionality
    is reached. This currently fails because draft child pages are listed
    on live requests to the live index. This was expected and is the bug
    that needs to be solved.
    
    I am testing draft requests (URL generated as reverse fro the wagtail
    admin view for the page that is draft) because that was easier to setup
    then preview requests. The rendered page should be the same.

--- bc/inlineindex/tests/fixtures.py
+++ bc/inlineindex/tests/fixtures.py
@@ -9,3 +9,10 @@ class InlineIndexFactory(wagtail_factories.PageFactory):
         model = models.InlineIndex
 
     title = factory.Sequence(lambda n: f"InlineIndex {n}")
+
+
+class InlineIndexChildFactory(wagtail_factories.PageFactory):
+    class Meta:
+        model = models.InlineIndexChild
+
+    title = factory.Sequence(lambda n: f"InlineIndexChild {n}")
--- bc/inlineindex/tests/tests.py
+++ bc/inlineindex/tests/tests.py
@@ -1,41 +1,89 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from wagtail.tests.utils import WagtailTestUtils
 
 from bc.home.models import HomePage
+from bc.inlineindex.tests.fixtures import InlineIndexFactory, InlineIndexChildFactory
 
-from bc.inlineindex.tests.fixtures import InlineIndexFactory
 
-
-class TestDisplayOfChildPages(TestCase):
-    def setUp(self):
+class TestDisplayOfChildPages(TestCase, WagtailTestUtils):
+    def setup_homepage(self):
         self.homepage = HomePage.objects.first()
-
         response = self.client.get(self.homepage.url)
         self.assertEqual(response.status_code, 200)
+
+    def setup_inline_index(self, live):
+        self.inline_index = InlineIndexFactory(
+            parent=self.homepage, title="The Example Index", live=live
+        )
+
+    def setup_inline_index_child(self, live):
+        self.inline_index_child = InlineIndexChildFactory(
+            parent=self.inline_index, title="The Example Child", live=live
+        )
+
+    def setUp(self):
+        self.setup_homepage()
 
     def test_live_request_to_live_index_success(self):
         """
         Just a sanity check.
         """
-        inline_index = InlineIndexFactory(
-            parent=self.homepage, title="The Example Index"
-        )
+        self.setup_inline_index(live=True)
 
-        response = self.client.get(inline_index.url)
+        response = self.client.get(self.inline_index.url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "The Example Index")
+        self.assertContains(response, self.inline_index.title)
 
     def test_live_request_to_draft_index_fails(self):
         """
         Just a sanity check.
         """
-        inline_index = InlineIndexFactory(
-            parent=self.homepage, title="The Example Index", live=False
-        )
+        self.setup_inline_index(live=False)
 
-        response = self.client.get(inline_index.url)
+        response = self.client.get(self.inline_index.url)
 
         self.assertEqual(response.status_code, 404)
 
-    def test_previewing_draft_index_shows_draft_children(self):
-        pass
+    def test_live_request_to_live_index_shows_live_child(self):
+        self.setup_inline_index(live=True)
+        self.setup_inline_index_child(live=True)
+
+        response = self.client.get(self.inline_index.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.inline_index.title)
+
+    def test_live_request_to_live_index_not_shows_draft_child(self):
+        self.setup_inline_index(live=True)
+        self.setup_inline_index_child(live=False)
+
+        response = self.client.get(self.inline_index.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.inline_index_child.title)
+
+    def test_draft_request_to_draft_index_success(self):
+        self.setup_inline_index(live=False)
+        self.login()
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:view_draft", args=(self.inline_index.id,))
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.inline_index.title)
+
+    def test_draft_request_to_draft_index_shows_draft_child(self):
+        self.setup_inline_index(live=False)
+        self.setup_inline_index_child(live=False)
+        self.login()
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:view_draft", args=(self.inline_index.id,))
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.inline_index_child.title)
