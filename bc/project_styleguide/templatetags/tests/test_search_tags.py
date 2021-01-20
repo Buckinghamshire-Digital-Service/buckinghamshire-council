54e5ee9dda20d26d26ae5c246c30bc80ba16402d
Tibor Leupold <tibor.leupold@torchbox.com>
2021-01-20 13:29:32 -0800

    Test is_news_page is False for InformationPage
    InformationPage here is a stand in for all other page types. It would
    just be too much to test all page types.

--- bc/project_styleguide/templatetags/tests/test_search_tags.py
+++ bc/project_styleguide/templatetags/tests/test_search_tags.py
@@ -1,8 +1,18 @@
 from django.test import TestCase
 
+from bc.home.models import HomePage
 from bc.project_styleguide.templatetags.search_tags import is_news_page
+from bc.standardpages.tests.fixtures import InformationPageFactory
 
 
 class TestIsNewsPageTag(TestCase):
+    def setUp(self):
+        self.homepage = HomePage.objects.first()
+
     def test_false_for_info_page(self):
-        is_news_page(None)
+        page = InformationPageFactory.build()
+        self.homepage.add_child(instance=page)
+
+        result = is_news_page(page)
+
+        self.assertIs(result, False)
