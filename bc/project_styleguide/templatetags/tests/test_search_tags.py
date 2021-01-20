9869a6c54f0f6c6f485a71b479ed6f4c3b400ce1
Tibor Leupold <tibor.leupold@torchbox.com>
2021-01-20 13:36:19 -0800

    Test is_news_page is true for NewsPage

--- bc/project_styleguide/templatetags/tests/test_search_tags.py
+++ bc/project_styleguide/templatetags/tests/test_search_tags.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from bc.home.models import HomePage
+from bc.news.tests.fixtures import NewsPageFactory
 from bc.project_styleguide.templatetags.search_tags import is_news_page
 from bc.standardpages.tests.fixtures import InformationPageFactory
 
@@ -16,3 +17,11 @@ class TestIsNewsPageTag(TestCase):
         result = is_news_page(page)
 
         self.assertIs(result, False)
+
+    def test_true_for_news_page(self):
+        page = NewsPageFactory.build()
+        self.homepage.add_child(instance=page)
+
+        result = is_news_page(page)
+
+        self.assertIs(result, True)
