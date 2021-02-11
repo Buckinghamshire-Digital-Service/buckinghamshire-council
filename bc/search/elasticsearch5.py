22d6b6318dbf904fd6eff2036129a7b8c0e65dab
Tibor Leupold <tibor.leupold@torchbox.com>
2021-02-11 02:23:29 -1000

    Reduce boost of news.NewsPage content type
    Before, News would show up fairly high in the search results for several
    search terms. These results could potentially be confusing as the news
    might be outdated.
    
    Multiple approaches (separate news search, no news in search, order news
    by date in results) where discussed. It was decided that news should
    still be discoverable through search but should rank much lower than
    other content, because of their time-limited value.
    
    To achieve a generally lower ranking of the NewsPage type, a query
    compiler mixin has been added that generally cuts the ranking score of
    news pages in half (negative boost factor 0.5).
    
    How NewsPages are handled in search results is likely to change in the
    future and needs to be informed by user research. For now, this is only
    a starting point.

--- bc/search/elasticsearch5.py
+++ bc/search/elasticsearch5.py
@@ -1,8 +1,40 @@
 import copy
 
-from wagtail.search.backends.elasticsearch5 import Elasticsearch5SearchBackend
+from wagtail.search.backends.elasticsearch5 import (
+    Elasticsearch5SearchQueryCompiler,
+    Elasticsearch5SearchBackend,
+)
 
 from bc.search.utils import get_synonyms
+
+
+class ReduceBoostSearchQueryCompilerMixin:
+    """
+    Mixin for Elasticsearch query compilers that reduce the boost of a given
+    content type.
+
+    All credit for this goes to @karl.
+    See also:
+    https://torchbox.slack.com/archives/C0251P48T/p1613040022098500?thread_ts=1613039057.098200&cid=C0251P48T
+
+    Currently the only content type that receives a negative boost is the
+    NewsPage content type. The type and the negative boost factor are hardcoded.
+
+    If necessary, this could potentially be opened for extension or made more
+    flexible.
+    See also:
+    https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-boosting-query.html
+
+    """
+
+    def get_inner_query(self):
+        return {
+            "boosting": {
+                "positive": super().get_inner_query(),
+                "negative": {"term": {"content_type": "news.NewsPage"}},
+                "negative_boost": 0.5,
+            }
+        }
 
 
 class SynonymSettingsMixin:
@@ -15,5 +47,12 @@ class SynonymSettingsMixin:
         }
 
 
+class SearchQueryCompiler(
+    ReduceBoostSearchQueryCompilerMixin, Elasticsearch5SearchQueryCompiler
+):
+    pass
+
+
 class SearchBackend(SynonymSettingsMixin, Elasticsearch5SearchBackend):
     settings = copy.deepcopy(Elasticsearch5SearchBackend.settings)
+    query_compiler_class = SearchQueryCompiler
