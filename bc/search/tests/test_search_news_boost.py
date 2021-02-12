477caec618927d58094ba90725e520b3fe4929d2
Tibor Leupold <tibor.leupold@torchbox.com>
2021-02-11 18:06:29 -1000

    Add boost factor tests
    Regression tests to make sure we are not removing this unwanted.
    
    Also, the tests can serve as a scaffolding for further changes once we
    know better what we want (in terms of how news pages should appear
    in the search results).

--- bc/search/tests/test_search_news_boost.py
+++ bc/search/tests/test_search_news_boost.py
@@ -4,6 +4,7 @@ from wagtail.core.models import Page
 
 from bc.news.tests.fixtures import NewsPageFactory
 from bc.home.models import HomePage
+from bc.standardpages.tests.fixtures import InformationPageFactory
 from .utils import get_search_settings_for_test, update_search_index
 
 
@@ -13,15 +14,33 @@ class TestNewsSearchBoostReduction(TestCase):
         self.homepage = HomePage.objects.first()
 
     def create_news_page(self):
-        news_page = NewsPageFactory.build(title="Hammer")
+        news_page = NewsPageFactory.build(title="Hammer News")
         self.homepage.add_child(instance=news_page)
         self.news_page = Page.objects.get(pk=news_page.id)
 
-    def test_news_page_in_search_results(self):
+    def create_info_page(self):
+        info_page = InformationPageFactory.build(title="Hammer Info")
+        self.homepage.add_child(instance=info_page)
+        self.info_page = Page.objects.get(pk=info_page.id)
+
+    @override_settings(SEARCH_BOOST_REDUCTION_FACTOR_NEWS_PAGE=1.0)
+    def test_news_before_info_when_full_boost_factor(self):
         self.create_news_page()
+        self.create_info_page()
         update_search_index()
 
-        search_results = Page.objects.search("Hammer")
+        search_results = Page.objects.search("Hammer News")
 
         self.assertEqual(self.news_page, list(search_results)[0])
+        self.assertEqual(self.info_page, list(search_results)[1])
 
+    @override_settings(SEARCH_BOOST_REDUCTION_FACTOR_NEWS_PAGE=0.1)
+    def test_info_before_news_despite_better_match_when_low_boost_factor(self):
+        self.create_news_page()
+        self.create_info_page()
+        update_search_index()
+
+        search_results = Page.objects.search("Hammer News")
+
+        self.assertEqual(self.info_page, list(search_results)[0])
+        self.assertEqual(self.news_page, list(search_results)[1])
