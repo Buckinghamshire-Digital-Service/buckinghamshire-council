9d205bc0da47e7ff685d39e0521a7dd91d480ded
Tibor Leupold <tibor.leupold@torchbox.com>
2021-02-17 23:16:33 -1000

    Fix black format

--- bc/utils/migrations/0009_systemmessagessettings_search_input_help_text.py
+++ bc/utils/migrations/0009_systemmessagessettings_search_input_help_text.py
@@ -7,13 +7,18 @@ import wagtail.core.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('utils', '0008_systemmessagessettings_body_no_search_results'),
+        ("utils", "0008_systemmessagessettings_body_no_search_results"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='systemmessagessettings',
-            name='search_input_help_text',
-            field=wagtail.core.fields.RichTextField(blank=True, help_text='This text will appear below the input field on the search page. You can use it to direct the users or suggest other pages where to find information.', null=True, verbose_name='Search Input Help Text'),
+            model_name="systemmessagessettings",
+            name="search_input_help_text",
+            field=wagtail.core.fields.RichTextField(
+                blank=True,
+                help_text="This text will appear below the input field on the search page. You can use it to direct the users or suggest other pages where to find information.",
+                null=True,
+                verbose_name="Search Input Help Text",
+            ),
         ),
     ]
