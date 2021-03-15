4936bffaf7e5c50eb5bc3b093589df06e6697087
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-15 02:31:53 -1000

    Add content blocks

--- bc/campaigns/models.py
+++ bc/campaigns/models.py
@@ -4,13 +4,26 @@ from wagtail.admin import edit_handlers
 from wagtail.core import blocks
 from wagtail.core import fields
 from wagtail.images import edit_handlers as image_handlers
+from wagtail.images import blocks as image_blocks
+
 
 from bc.utils.models import BasePage
+
+
+class SectionContentBlock(blocks.StructBlock):
+    image = image_blocks.ImageChooserBlock()
+    subheading = blocks.CharBlock(
+        max_length=250,
+        template="patterns/molecules/streamfield/blocks/subheading_block.html",
+    )
+    paragraph = blocks.RichTextBlock(features=["link"])
 
 
 class SectionBlock(blocks.StructBlock):
     heading = blocks.CharBlock(form_classname="full title")
     intro = blocks.RichTextBlock(features=["link"])
+
+    content = blocks.ListBlock(SectionContentBlock())
 
 
 class CampaignPage(BasePage):
