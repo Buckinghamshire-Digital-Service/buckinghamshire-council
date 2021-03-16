16298aba9ded0525604df21ea97ffbd5201cee11
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-15 18:00:54 -1000

    Add styling for ImageOrEmbedBlock in section content

--- bc/campaigns/blocks.py
+++ bc/campaigns/blocks.py
@@ -4,7 +4,9 @@ from bc.utils.blocks import ImageOrEmbedBlock
 
 
 class SectionContentBlock(blocks.StructBlock):
-    image_or_embed = ImageOrEmbedBlock()
+    image_or_embed = ImageOrEmbedBlock(
+        form_classname="struct-block c-sf-block c-sf-block__content-inner"
+    )
     subheading = blocks.CharBlock(
         max_length=250,
         template="patterns/molecules/streamfield/blocks/subheading_block.html",
