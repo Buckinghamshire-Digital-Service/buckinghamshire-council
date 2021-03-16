1995d7e7af3b9e1dabb234e13c6e7d8feac5518c
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-16 00:01:01 -1000

    Add campaign index page

--- bc/campaigns/models.py
+++ bc/campaigns/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 from wagtail.admin import edit_handlers
 from wagtail.core import fields
+from wagtail.core import models as wt_models
 from wagtail.images import edit_handlers as image_handlers
 
 from bc.campaigns.blocks import SectionBlock
 from bc.utils.models import BasePage
 
 
+class CampaignIndexPage(wt_models.Page):
+    parent_page_types = ["home.homepage"]
+    subpage_types = ["campaigns.campaignpage"]
+    max_count = 1
+
+
 class CampaignPage(BasePage):
+    parent_page_types = ["campaigns.campaignindexpage"]
+
     template = "patterns/pages/campaigns/campaign_page.html"
 
     intro = fields.RichTextField(features=("link"))
