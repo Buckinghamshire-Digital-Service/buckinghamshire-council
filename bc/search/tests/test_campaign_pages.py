917cd2bc4956179ab033c93c8b8a53f4c7943da4
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-13 16:00:01 -1000

    Add test to check campaign page in serach

---
+++ bc/search/tests/test_campaign_pages.py
@@ -0,0 +1,19 @@
+from django import urls, test
+
+from wagtail.core.models import Page
+
+from bc.campaigns.tests.fixtures import CampaignPageFactory
+
+
+class CampaignPagesNotIncludedTest(test.TestCase):
+    def test_campaign_page_not_in_search_results(self):
+        search_word = "screwdrivers"
+        campaign_page = CampaignPageFactory.create(title=search_word)
+
+        response = self.client.get(
+            urls.reverse("search") + "?query={query}".format(query=search_word)
+        )
+
+        self.assertNotIn(
+            Page.objects.get(pk=campaign_page.pk), response.context["search_results"]
+        )
