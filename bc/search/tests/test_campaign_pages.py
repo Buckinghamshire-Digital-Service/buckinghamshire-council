411e9e19fda9bfeb5cfa40f6b8e5764539ec6257
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-21 17:14:26 -1000

    Add sanity test to check infopage not excluded
    Before, there was only a test checking that a campaign page of a particular
    title was not present in the search results.
    
    A new test is added to make sure that other pages (like an InformationPage) of
    the same title are still present in the search results.

--- bc/search/tests/test_campaign_pages.py
+++ bc/search/tests/test_campaign_pages.py
@@ -3,6 +3,7 @@ from django import test, urls
 from wagtail.core.models import Page
 
 from bc.campaigns.tests.fixtures import CampaignPageFactory
+from bc.standardpages.tests.fixtures import InformationPageFactory
 
 
 class CampaignPagesNotIncludedTest(test.TestCase):
@@ -16,4 +17,21 @@ class CampaignPagesNotIncludedTest(test.TestCase):
 
         self.assertNotIn(
             Page.objects.get(pk=campaign_page.pk), response.context["search_results"]
+        )
+
+    def test_campaign_page_not_in_search_result_but_info_with_same_title_is(self):
+        """Sanity check."""
+        search_word = "screwdrivers"
+        campaign_page = CampaignPageFactory.create(title=search_word)
+        information_page = InformationPageFactory.create(title=search_word)
+
+        response = self.client.get(
+            urls.reverse("search") + "?query={query}".format(query=search_word)
+        )
+
+        self.assertNotIn(
+            Page.objects.get(pk=campaign_page.pk), response.context["search_results"]
+        )
+        self.assertIn(
+            Page.objects.get(pk=information_page.pk), response.context["search_results"]
         )
