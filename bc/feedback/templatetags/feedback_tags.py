cfdf6828d6a3fabe0ee80abfedb17bb8f46e565f
Tibor Leupold <tibor.leupold@torchbox.com>
2021-05-10 22:14:12 -1000

    Add yes and no forms to page with template tag

---
+++ bc/feedback/templatetags/feedback_tags.py
@@ -0,0 +1,26 @@
+from django import template
+
+from bc.feedback.forms import UsefulnessFeedbackForm
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    "patterns/molecules/feedback-widget/feedback-widget.html",
+    takes_context=True
+)
+def feedback_widget(context):
+    page = context.get("page")
+    extra_context = {}
+
+    if page:
+        extra_context["yes_form"] = UsefulnessFeedbackForm(initial={
+            "useful": True,
+            "page": page,
+        })
+        extra_context["no_form"] = UsefulnessFeedbackForm(initial={
+            "useful": False,
+            "page": page,
+        })
+
+    return extra_context
