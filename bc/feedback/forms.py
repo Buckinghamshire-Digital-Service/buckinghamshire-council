a87260ee95278901694e103e77d70859608c143f
Tibor Leupold <tibor.leupold@torchbox.com>
2021-05-11 01:24:38 -1000

    Hide form inputs

--- bc/feedback/forms.py
+++ bc/feedback/forms.py
@@ -7,3 +7,10 @@ class UsefulnessFeedbackForm(forms.ModelForm):
     class Meta:
         model = UsefulnessFeedback
         fields = ("page", "useful")
+        # The basic yes/no form are always prefilled and do not require the user to fill
+        # any form. The user only submits one of the two forms (one prefilled with
+        # useful=False, the other with useful=True).
+        widgets = {
+            "page": forms.HiddenInput(),
+            "useful": forms.HiddenInput(),
+        }
