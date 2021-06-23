59c95bd9746bd28103f6b3383903801b674a755a
Nick Smith <nick.smith@torchbox.com>
2021-06-23 11:11:51 +0100

    Add feature flag for page footer feedback widget

--- bc/settings/base.py
+++ bc/settings/base.py
@@ -794,3 +794,8 @@ RESPOND_GET_FIELDS_WEBSERVICE = env.get("RESPOND_GET_FIELDS_WEBSERVICE")
 
 # MapIt.MySociety API credentials
 MAPIT_API_KEY = env.get("MAPIT_API_KEY")
+
+# Feature flags
+ENABLE_FEEDBACK_WIDGET = (  # Page usefulness and comment forms in the footer
+    env.get("ENABLE_FEEDBACK_WIDGET", "true").lower().strip() == "true"
+)
