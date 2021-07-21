f1db01b386d5c4f3f606416a6e3dd35f8268895c
Nick Smith <nick.smith@torchbox.com>
2021-07-21 15:02:15 +0100

    Make Aptean Respond form pages routable
    This adds 'information page' like functionality to
    ApteanRespondCaseFormPages, and moves their form view to the `/form/`
    route. This means that a parent information page is not necessary, that
    the form route does not show up in search results, and that users do not
    arrive directly at the form without seeing the guidance first.

---
+++ bc/cases/blocks.py
@@ -0,0 +1,25 @@
+from wagtail.core import blocks
+
+from bc.utils.blocks import StoryBlock
+
+
+class FormButtonBlock(blocks.StructBlock):
+    text = blocks.CharBlock(form_classname="title", help_text="The button label")
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context["text"] = value["text"]
+
+        # Hard-code the URL of the button
+        page = parent_context["page"]
+        context["value"]["url"] = page.reverse_subpage("form_route")
+
+        return context
+
+    class Meta:
+        icon = "success"
+        template = "patterns/molecules/streamfield/blocks/button_block.html"
+
+
+class CaseFormStoryBlock(StoryBlock):
+    form_link_button = FormButtonBlock()
