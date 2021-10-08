9f0518a2d8fc4c7160ae3686c7c720ff6fcaa284
Tibor Leupold <tibor.leupold@torchbox.com>
2021-10-08 11:31:53 +0200

    Define Python version for AWS CLI install
    The installation was failing before because it was defaulting to run
    with the Python 2.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -68,7 +68,7 @@ rm -rf /tmp/awscli-bundle || true
 rm -rf /tmp/awscli-bundle.zip || true
 curl -sSL "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
 unzip -q /tmp/awscli-bundle.zip -d /tmp
-/tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+python3 /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 # Add a couple of aliases to manage.py into .bashrc
 cat << EOF >> /home/vagrant/.bashrc
