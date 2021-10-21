e29845282357af2f7389833fa44908e45fe8893f
Shohan <shohanduttaroy99@gmail.com>
2021-10-21 16:28:59 +0530

    refactor(step-by-step-index): Break step by step index template into several block templates

--- bc/step_by_step/models.py
+++ bc/step_by_step/models.py
@@ -15,20 +15,31 @@ from bc.utils.models import BasePage
 
 
 class StepInformationBlock(StreamBlock):
-    paragraph = TextBlock()
+    paragraph = TextBlock(
+        template="patterns/molecules/step_by_step/blocks/paragraph-block.html",
+    )
     external_link = StructBlock(
-        [("url", URLBlock()), ("title", CharBlock())], icon="link",
+        [("url", URLBlock()), ("title", CharBlock())],
+        icon="link",
+        template="patterns/molecules/step_by_step/blocks/external-link-block.html",
     )
 
     internal_link = StructBlock(
         [("page", PageChooserBlock()), ("title", CharBlock(required=False))],
         icon="link",
+        template="patterns/molecules/step_by_step/blocks/internal-link-block.html",
     )
+
+    class Meta:
+        template = "patterns/molecules/step_by_step/blocks/step-block.html"
 
 
 class StepBlock(StructBlock):
     heading = TextBlock()
     information = StepInformationBlock()
+
+    class Meta:
+        template = "patterns/molecules/step_by_step/blocks/step-block.html"
 
 
 class StepByStepIndexPage(BasePage):
