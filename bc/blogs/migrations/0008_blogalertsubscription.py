bebd1a132bada46d98b2e56ffaa8677e64f54ed7
Victor Miti <victor.miti@torchbox.com>
2022-06-30 14:05:39 +0200

    Re-order migration file after rebasing onto integration/blog-subscriptions

--- bc/blogs/migrations/0008_blogalertsubscription.py
+++ bc/blogs/migrations/0008_blogalertsubscription.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("blogs", "0003_featured_blogpost"),
+        ("blogs", "0007_blogglobalhomepage"),
     ]
 
     operations = [
