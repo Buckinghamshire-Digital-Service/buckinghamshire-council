fa2d41f18c1d5424854674805736f9345ca17076
Victor Miti <victor.miti@torchbox.com>
2022-07-08 16:01:57 +0200

    Fix django.db.transaction.TransactionManagementError
    django.db.transaction.TransactionManagementError: select_for_update cannot be used outside of a transaction.

--- bc/blogs/management/commands/alert_subscribers.py
+++ bc/blogs/management/commands/alert_subscribers.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from bc.blogs.models import NotificationRecord
 from bc.blogs.utils import alert_subscribed_users
@@ -12,15 +13,16 @@ class Command(BaseCommand):
     help = "Notify subscribed users of newly published blog posts"
 
     def handle(self, *args, **options):
-
-        pending_notifications = NotificationRecord.objects.select_for_update(
-            skip_locked=True
-        ).filter(was_sent=False)
-        if pending_notifications:
-            for record in pending_notifications:
-                page = record.blog_post
-                alert_subscribed_users(page.pk)
-                record.was_sent = True
-                record.save()
-        else:
-            self.stdout.write("There are no pending subscription alert notifications")
+        with transaction.atomic():
+            pending_notifications = NotificationRecord.objects.select_for_update(
+                skip_locked=True
+            ).filter(was_sent=False)
+            if pending_notifications:
+                for record in pending_notifications:
+                    alert_subscribed_users(record.blog_post_id)
+                    record.was_sent = True
+                    record.save()
+            else:
+                self.stdout.write(
+                    "There are no pending subscription alert notifications"
+                )
