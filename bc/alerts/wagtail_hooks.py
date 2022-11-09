8827acc6b30b5e1d7e5f18ce8d2403e75ca19ec4
Shohan Dutta Roy <shohan.duttaroy@torchbox.com>
2022-11-09 18:58:49 +0530

    feat(alerts): Allow alert page to be null, and add search fields for alert modeladmin

--- bc/alerts/wagtail_hooks.py
+++ bc/alerts/wagtail_hooks.py
@@ -7,6 +7,7 @@ class AlertModelAdmin(ModelAdmin):
     model = Alert
     menu_icon = "warning"
     list_display = ("title", "alert_level", "page")
+    search_fields = ["title", "page__title"]
 
 
 modeladmin_register(AlertModelAdmin)
