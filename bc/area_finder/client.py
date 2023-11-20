e48c3aa43fd47d6a523de73fe93f80c8b64ec717
Lauren <lauren.parsons@torchbox.com>
2023-11-20 10:32:14 +0000

    Implement automatic retries for failed connections

--- bc/area_finder/client.py
+++ bc/area_finder/client.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 
-import requests
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 
 class BucksMapsClient:
@@ -42,8 +44,30 @@ class BucksMapsClient:
         "f": "pjson",
     }
 
+    def __init__(self):
+        """
+        Setting up a session with retries due to Sentry ConnectionError seen in production;
+
+        - backoff_factor is increase in sleep time between retries (10%)
+        - status_forcelist is the list of status codes to retry on
+        - allowed_methods is the set of uppercased HTTP method verbs that we should retry on.
+
+        Here we will retry the request 3 times if it fails with a 502, 503 or 504 error
+        with an increasing length of time between retries.
+        """
+        self.session = Session()
+
+        retries = Retry(
+            total=3,
+            backoff_factor=0.1,
+            status_forcelist=[502, 503, 504],
+            allowed_methods={"POST"},
+        )
+
+        self.session.mount(self.base_url, adapter=HTTPAdapter(max_retries=retries))
+
     def _post(self, data):
-        response = requests.post(
+        response = self.session.post(
             self.base_url,
             data=data,
             timeout=settings.BUCKS_MAPS_CLIENT_API_TIMEOUT_SECONDS,
