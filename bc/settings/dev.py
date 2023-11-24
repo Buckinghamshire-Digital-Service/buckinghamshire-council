9bce4568652872b7b546bef673ac3f8fe686a0aa
Katherine Domingo <katherine.domingo@torchbox.com>
2023-11-24 11:11:41 +0800

    Replace captcha with django_recaptcha following the package namespace rename on django-recaptcha 4.0.0

--- bc/settings/dev.py
+++ bc/settings/dev.py
@@ -14,7 +14,7 @@ PATTERN_LIBRARY_ENABLED = True
 ALLOWED_HOSTS = ["*"]
 
 # Silence recapcha key warning
-SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+SILENCED_SYSTEM_CHECKS = ["django_recaptcha.recaptcha_test_key_error"]
 
 # Allow requests from the local IPs to see more debug information.
 INTERNAL_IPS = ("127.0.0.1", "10.0.2.2")
