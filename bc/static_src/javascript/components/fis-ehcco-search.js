9098cfbd2f231b9c721e450bedcdd7d1f7d3c42b
Jane <jane.hughes@torchbox.com>
2024-06-12 12:03:30 +0100

    finish styling and tidy up js

--- bc/static_src/javascript/components/fis-ehcco-search.js
+++ bc/static_src/javascript/components/fis-ehcco-search.js
@@ -16,10 +16,16 @@ class EHCCOSearch {
         this.getCorrespondingEhcCoUrl = this.node.getAttribute(
             'data-ehc-co-url',
         );
-        // this.ehccoSchool = this.node.querySelector(
-        //     '[data-autocomplete-school]',
-        // );
+        this.ehccoSchool = this.node.querySelector(
+            '[data-autocomplete-school]',
+        );
         this.ehccoName = this.node.querySelector('[data-autocomplete-name]');
+
+        this.state = {
+            school: null,
+            name: null,
+        };
+
         this.bindEvents();
     }
 
@@ -27,22 +33,27 @@ class EHCCOSearch {
         accessibleAutocomplete({
             element: this.searchWrapper,
             id: 'school-search', // To match the existing HTML element
+            inputClasses: 'ehcco-search__input',
+            menuClasses: 'ehcco-search__options',
+            showNoOptionsFound: false,
             source: (query, populateResults) => {
                 this.fetchMatchingSchools(query)
                     .then((results) => {
                         populateResults(
                             results.map((school) => ({
                                 id: school.id,
-                                label: school.name,
+                                label: school.text,
                             })),
                         );
                     })
+
                     .catch((error) =>
                         console.error('error getting schools:', error),
                     );
             },
             onConfirm: (selected) => {
                 if (selected) {
+                    this.state.school = selected.label;
                     this.fetchSchoolEHCCO(selected.id);
                 }
             },
@@ -56,14 +67,12 @@ class EHCCOSearch {
     fetchMatchingSchools(query) {
         return fetch(`${this.getMatchingSchoolsUrl}?q=${query}`)
             .then((response) => {
-                console.log('getMatchingSchoolsUrl response:', response);
                 if (!response.ok) {
                     throw new Error('response not ok');
                 }
                 return response.json();
             })
             .then((data) => {
-                console.log('getMatchingSchoolsUrl data:', data);
                 return data;
             });
     }
@@ -71,21 +80,23 @@ class EHCCOSearch {
     fetchSchoolEHCCO(schoolId) {
         fetch(`${this.getCorrespondingEhcCoUrl}?school_id=${schoolId}`)
             .then((response) => {
-                console.log('getCorrespondingEhcCoUrl response:', response); // Debugging
                 if (!response.ok) {
                     throw new Error('response not ok');
                 }
                 return response.json();
             })
             .then((data) => {
-                console.log('getCorrespondingEhcCoUrl data:', data);
-                this.displayEHCCO(data.name);
+                this.state.name = data.name;
+                this.displayResult();
             })
             .catch((error) => console.error('error getting EHCCO', error));
     }
 
-    displayEHCCO(name) {
-        this.ehccoName.textContent = name;
+    displayResult() {
+        if (this.state.school && this.state.name) {
+            this.ehccoSchool.textContent = this.state.school;
+            this.ehccoName.textContent = this.state.name;
+        }
     }
 }
 
